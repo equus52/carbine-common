d60568c992d42e2221b16183ed04ab6a71e92867
hiro <miyamura52@gmail.com>
2014-04-07 01:18:00 +0900

    remove nullable

--- src/main/java/equus/carbine/exception/NoSuchPropertyException.java
+++ src/main/java/equus/carbine/exception/NoSuchPropertyException.java
@@ -1,23 +1,20 @@
 package equus.carbine.exception;
 
-import javax.annotation.Nullable;
-
 /**
  * Exception thrown when public field or setter is not found.
  */
 @SuppressWarnings("serial")
 public final class NoSuchPropertyException extends RuntimeException {
 
-    public NoSuchPropertyException(final String property, @Nullable final Throwable cause) {
+    public NoSuchPropertyException(final String property, final Throwable cause) {
         super("Not Found property. name: " + property, cause);
     }
 
     public NoSuchPropertyException(final String property) {
-        this(property, null);
+        super(property, null);
     }
 
     public NoSuchPropertyException(final Throwable cause) {
         super(cause);
     }
-
 }
