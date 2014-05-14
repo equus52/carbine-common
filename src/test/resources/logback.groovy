4fdfc479c14e543927d6486b84d6c68312e74600
hiro <miyamura52@gmail.com>
2014-05-14 19:28:10 +0900

    modify logback setting

--- src/test/resources/logback.groovy
+++ src/test/resources/logback.groovy
@@ -1,18 +1,22 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+import ch.qos.logback.core.rolling.RollingFileAppender
+import ch.qos.logback.core.rolling.SizeAndTimeBasedFNATP
+import ch.qos.logback.core.rolling.TimeBasedRollingPolicy
+
 def appName = "carbine-common"
 def patternStr = "%d{yyyy/MM/dd HH:mm:ss:SSS} [%level] [%thread] [%logger{0}] %msg%n"
 
 appender("FILE", RollingFileAppender) {
-  encoder(PatternLayoutEncoder) {
-    pattern = "${patternStr}"
-  }
+  encoder(PatternLayoutEncoder) { pattern = "${patternStr}" }
   rollingPolicy(TimeBasedRollingPolicy) {
-    FileNamePattern = "log/${appName}.%d{yyyy-MM-dd}.log"
+    fileNamePattern = "log/${appName}.%d{yyyy-MM-dd}.%i.log"
+    timeBasedFileNamingAndTriggeringPolicy(SizeAndTimeBasedFNATP){ maxFileSize = "100MB" }
+    maxHistory = 30
   }
 }
 
 appender("CONSOLE", ConsoleAppender) {
-  encoder(PatternLayoutEncoder) {
-    pattern = "${patternStr}"
-  }
+  encoder(PatternLayoutEncoder) { pattern = "${patternStr}" }
 }
 root(INFO, ["CONSOLE", "FILE"])
