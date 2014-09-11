1bfadef722559916527d0ce5f01e088edc184214
hiro <miyamura52@gmail.com>
2014-09-12 00:37:03 +0900

    add Optional instanceOf

--- src/main/java/equus/carbine/util/GenericsUtils.java
+++ src/main/java/equus/carbine/util/GenericsUtils.java
@@ -1,18 +1,27 @@
 package equus.carbine.util;
 
 import java.util.List;
+import java.util.Optional;
 
 public final class GenericsUtils {
 
-    private GenericsUtils() {}
+  private GenericsUtils() {}
 
-    @SuppressWarnings("unchecked")
-    public static <T> T cast(Object obj) {
-        return (T) obj;
-    }
+  @SuppressWarnings("unchecked")
+  public static <T> T cast(Object obj) {
+    return (T) obj;
+  }
 
-    @SuppressWarnings("unchecked")
-    public static <T> T[] toArray(List<T> list) {
-        return (T[]) list.toArray();
+  @SuppressWarnings("unchecked")
+  public static <T> T[] toArray(List<T> list) {
+    return (T[]) list.toArray();
+  }
+
+  public static <S, T extends S> Optional<T> instanseOf(S obj, Class<T> clazz) {
+    if (clazz.isInstance(obj)) {
+      return Optional.ofNullable((T) obj);
+    } else {
+      return Optional.empty();
     }
+  }
 }
--- src/test/java/equus/carbine/util/GenericsUtilsTest.java
+++ src/test/java/equus/carbine/util/GenericsUtilsTest.java
@@ -1,18 +1,38 @@
 package equus.carbine.util;
 
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.junit.Test;
 
 public class GenericsUtilsTest {
 
-    @Test
-    public void testPrivateConstractor() {
-        TestUtils.privateConstractor(GenericsUtils.class).newInstance();
-    }
+  @Test
+  public void testPrivateConstractor() {
+    TestUtils.privateConstractor(GenericsUtils.class).newInstance();
+  }
 
-    @SuppressWarnings("unused")
-    @Test
-    public void testCast() {
-        Object obj = "";
-        String str = GenericsUtils.cast(obj);
+  @SuppressWarnings("unused")
+  @Test
+  public void testCast() {
+    Object obj = "";
+    String str = GenericsUtils.cast(obj);
+  }
+
+  @Test
+  public void test_instanseOf() {
+    {
+      Number num = BigDecimal.ONE;
+      Optional<BigDecimal> o = GenericsUtils.instanseOf(num, BigDecimal.class);
+      o.ifPresent(b -> System.out.println(b.toPlainString()));
+      assertTrue(o.isPresent());
     }
+    {
+      Object num = 1;
+      Optional<BigDecimal> o = GenericsUtils.instanseOf(num, BigDecimal.class);
+      o.ifPresent(b -> fail());
+    }
+  }
 }
